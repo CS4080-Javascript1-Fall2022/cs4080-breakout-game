6ac6ef186e2fa15e32f56a5f56181c202be32939
Kenneth Shuto <kennethshuto@gmail.com>
2022-12-01 23:08:55 -0800

    step 6

--- breakout.js
+++ breakout.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;                           //ball x pos
 let y = canvas.height - 30;                         //ball y pos
 
-let speed = 6;                                      //ball speed
+let speed = 4;                                      //ball speed
 let dx = speed;
 let dy = -speed;
 
@@ -14,8 +14,26 @@ const ballRadius = 10;                              //ball radius
 
 const paddleHeight = 10;                            //paddle height
 const paddleWidth = 100;                            //paddle width
+const paddleGrace = 10;
 let paddleX = (canvas.width - paddleWidth) / 2;     //paddle position
 let rightPressed = leftPressed = false;             //Controls user input
+
+//Brick information
+const brickRowCount = 3;
+const brickColumnCount = 10;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 17;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
 
 //draws the ball onto the canvas
 function drawBall() {
@@ -35,11 +53,29 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+//draws the bricks onto the canvas
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+      for (let r = 0; r < brickRowCount; r++) {
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+}
+
 //Main function to draw everything into the canvas
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
+    drawBricks();
 
     //Bounce ball off the top and bottom
     if (y + dy > canvas.height || y + dy < 0) {
@@ -53,7 +89,7 @@ function draw() {
     if (y + dy < ballRadius) {
         dy = -dy;
     } else if (y + dy > canvas.height - ballRadius) {
-        if (x > paddleX && x < paddleX + paddleWidth) {
+        if (x > paddleX - paddleGrace && x < paddleX + paddleGrace + paddleWidth) {
           dy = -dy;
         } else {
           alert("GAME OVER");
